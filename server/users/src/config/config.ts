fb12efb9a97310192aa5141e9d4700acfed73287
Naylepsh <naylepsh@gmail.com>
2020-07-20 10:32:28 +0200

    setup config

---
+++ server/users/src/config/config.ts
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const env = process.env.NODE_ENV;
+
+console.log(env);
+
+const dev = {
+  app: {
+    port: process.env.DEV_PORT || 1337,
+  },
+};
+
+const test = {
+  app: {
+    port: process.env.TEST_PORT || 1337,
+  },
+};
+
+const cfg = {
+  dev,
+  test,
+};
+
+export const config = cfg[env];
