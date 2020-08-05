89769783cc10579e7d411813c8a68a6c26e68790
Naylepsh <naylepsh@gmail.com>
2020-08-05 16:28:43 +0200

    add validation to lecturers;

--- server/app/src/dtos/lecturers/save-lecturer.dto.ts
+++ server/app/src/dtos/lecturers/save-lecturer.dto.ts
@@ -1,8 +1,22 @@
+import { IsOptional, IsUUID, IsString, IsEmail } from 'class-validator';
+
 export class SaveLecturerDto {
+  @IsUUID('4')
+  @IsOptional()
   id?: string;
+
+  @IsString()
   firstName: string;
+
+  @IsString()
   lastName: string;
+
+  @IsString()
   phoneNumber: string;
+
+  @IsEmail()
   email: string;
+
+  @IsUUID('4', { each: true })
   groups: string[];
 }
