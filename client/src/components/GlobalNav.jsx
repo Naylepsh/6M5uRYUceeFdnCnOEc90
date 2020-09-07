225cd2f6dae6a069ce4d415a707a824548bb7206
miloczek <33574399+miloczek@users.noreply.github.com>
2020-09-07 14:48:13 +0200

    add new tab in GlobalNav

--- client/src/components/GlobalNav.jsx
+++ client/src/components/GlobalNav.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "./../utils/react-router-next";
-import { FaCalendarAlt, FaBell } from "react-icons/fa";
+import { FaCalendarAlt, FaBell, FaUserEdit } from "react-icons/fa";
 import "./GlobalNav.css";
 
 export default function GlobalNav() {
@@ -8,6 +8,9 @@ export default function GlobalNav() {
     <nav className="GlobalNav">
       <Link href="/">
         <FaCalendarAlt /> <span>Kalendarz</span>
+      </Link>
+      <Link href="/manager">
+        <FaUserEdit /> <span>Edytuj dane</span>
       </Link>
       <Link href="/feed">
         <FaBell /> <span>Ostatnie notatki</span>
