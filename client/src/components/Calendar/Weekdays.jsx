6fdbe8ae6d36aa425ee04a33f4b4e48d79b482a6
Naylepsh <naylepsh@gmail.com>
2020-09-08 20:52:18 +0200

    change day name length depending on size

--- client/src/components/Calendar/Weekdays.jsx
+++ client/src/components/Calendar/Weekdays.jsx
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weekdays.css";
-//Simpy static elements with polish day names
+import { useEffect, useCallback } from "react";
+
+const weekdaysFullNames = [
+  "poniedziałek",
+  "wtorek",
+  "środa",
+  "czwartek",
+  "piątek",
+  "sobota",
+  "niedziela",
+];
 
 export function Weekdays() {
+  const [weekdays, setWeekdays] = useState(weekdaysFullNames);
+
+  const shortenNames = useCallback(() => {
+    const newWeekdays =
+      window.innerWidth < 768
+        ? weekdays.map((day) => day.slice(0, 3))
+        : weekdaysFullNames;
+    setWeekdays(newWeekdays);
+  }, []);
+
+  useEffect(() => {
+    shortenNames();
+    window.matchMedia("(max-width: 768px)").addListener(shortenNames);
+  }, [shortenNames]);
+
   return (
     <div className="Weekdays">
-      <div>poniedziałek</div>
-      <div>wtorek</div>
-      <div>środa</div>
-      <div>czwartek</div>
-      <div>piątek</div>
-      <div>sobota</div>
-      <div>niedziela</div>
+      {weekdays.map((day, i) => (
+        <div key={i}>{day}</div>
+      ))}
     </div>
   );
 }
