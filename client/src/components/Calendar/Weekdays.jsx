bbd4e31c2743b8ea696bcf82829f6849359cfef5
Naylepsh <naylepsh@gmail.com>
2020-09-08 22:22:46 +0200

    something

--- client/src/components/Calendar/Weekdays.jsx
+++ client/src/components/Calendar/Weekdays.jsx
@@ -1,34 +1,9 @@
 import React, { useState } from "react";
 import "./Weekdays.css";
-import { useEffect, useCallback } from "react";
 
-const weekdaysFullNames = [
-  "poniedziałek",
-  "wtorek",
-  "środa",
-  "czwartek",
-  "piątek",
-  "sobota",
-  "niedziela",
-];
+const weekdays = ["pon", "wt", "śr", "czw", "pt", "sb", "nd"];
 
 export function Weekdays() {
-  const [weekdays, setWeekdays] = useState(weekdaysFullNames);
-  const maxWidth = 768;
-
-  const shortenNames = useCallback(() => {
-    const newWeekdays =
-      window.innerWidth < maxWidth
-        ? weekdays.map((day) => day.slice(0, 3))
-        : weekdaysFullNames;
-    setWeekdays(newWeekdays);
-  }, []);
-
-  useEffect(() => {
-    shortenNames();
-    window.matchMedia(`(max-width: ${maxWidth}px)`).addListener(shortenNames);
-  }, [shortenNames]);
-
   return (
     <div className="Weekdays">
       {weekdays.map((day, i) => (
